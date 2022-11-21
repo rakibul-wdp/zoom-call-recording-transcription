712ca5ae033ca8b3dacb18a5dd10231fcd13d600
rakibul-wdp <rakibul.wdp@gmail.com>
2022-11-22 05:45:00 +0600

    create topic component

---
+++ src/components/topic/Topic.jsx
@@ -0,0 +1,253 @@
+import React from 'react'
+import './topic.css'
+const Topic = () => {
+    return (
+        <div className='topicWrapper'>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="topicWrapperDiv">
+                <div className="topicWrapperDivHead">Competitors- Demo</div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+                <div className="topicWrapperDivGraph">
+                    <div className="topicWrapperDivGraphLabel">Name</div>
+                    <div className="topicWrapperDivProgressBar">
+                        <div className="progressMade"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Topic
